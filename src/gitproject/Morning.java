a908bd189b7f8ba9331290a7e7f99c12793c1710
aayad72 <33642806+aayad72@users.noreply.github.com>
2018-03-10 14:37:09 -0500

    after edition

--- src/gitproject/Morning.java
+++ src/gitproject/Morning.java
@@ -4,7 +4,8 @@ public class Morning {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello there");
-		System.out.println("How are you ?");
+		System.out.println("How are you ? ok");
+		
 	}
 
 }
